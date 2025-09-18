5b275d937055c1c702e7081aa8644d4b64ee30c3
Thiago Seixas <ticolls@hotmail.com>
2025-09-18 11:55:48 -0300

    feat: add presence service

---
+++ src/presence/presence.service.ts
@@ -0,0 +1,64 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreatePresenceDto } from './dto/create-presence.dto';
+import { PresenceRepository } from './presence.repository';
+import { PresenceResponseDto } from './dto/presence-response.dto';
+import { handlePrismaError } from 'src/exceptions/handlePrismaError';
+import { PresenceQueryDto } from './dto/presence-query.dto';
+import { PersonRepository } from 'src/person/person.repository';
+import { AreaRepository } from 'src/area/area.repository';
+
+@Injectable()
+export class PresenceService {
+  constructor(
+    readonly presenceRepository: PresenceRepository,
+    readonly personRepository: PersonRepository,
+    readonly areaRepository: AreaRepository,
+  ) {}
+  async create(createPresenceDto: CreatePresenceDto) {
+    try {
+      const areaExists = await this.areaRepository.exists(
+        createPresenceDto.areaId,
+      );
+
+      if (!areaExists) {
+        throw new BadRequestException('Não existe uma área com esse ID');
+      }
+
+      const personExists = await this.personRepository.exists(
+        createPresenceDto.personId,
+      );
+
+      if (!personExists) {
+        throw new BadRequestException('Não existe uma pessoa com esse ID');
+      }
+
+      const now = new Date();
+      const occurredAt = createPresenceDto.occurredAt
+        ? new Date(createPresenceDto.occurredAt)
+        : null;
+
+      if (occurredAt && occurredAt > now) {
+        throw new BadRequestException(
+          'Não é possível cadastrar uma presença no futuro',
+        );
+      }
+
+      const createdPresence =
+        await this.presenceRepository.create(createPresenceDto);
+
+      return new PresenceResponseDto(createdPresence);
+    } catch (error) {
+      handlePrismaError(error, 'Presença');
+    }
+  }
+
+  async findAll(filters: PresenceQueryDto) {
+    try {
+      const presences = await this.presenceRepository.findMany(filters);
+
+      return presences.map((presence) => new PresenceResponseDto(presence));
+    } catch (error) {
+      handlePrismaError(error, 'Presença');
+    }
+  }
+}
