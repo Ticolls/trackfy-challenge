9344d9808b5d1af3924abbf80a1fe92a00602255
Thiago Seixas <ticolls@hotmail.com>
2025-09-18 11:56:42 -0300

    feat: add modules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PersonModule } from './person/person.module';
+import { AreaModule } from './area/area.module';
+import { PresenceModule } from './presence/presence.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PersonModule, AreaModule, PresenceModule, PrismaModule],
 })
 export class AppModule {}
